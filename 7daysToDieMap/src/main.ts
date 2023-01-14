5f165d846f2dd48c22c75cb4ebce54ba89511ec3
Dustin <Dustin@d-welborn02.na.corp.samsungelectronics.net>
2023-01-14 17:42:45 -0500

    setup vue 3 and firebase hosting

---
+++ 7daysToDieMap/src/main.ts
@@ -0,0 +1,38 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/main.css";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB7tRHEl6hE6OW_tEG4RqP5w3T4UoBuFG4",
+  authDomain: "daysmap-f3b25.firebaseapp.com",
+  projectId: "daysmap-f3b25",
+  storageBucket: "daysmap-f3b25.appspot.com",
+  messagingSenderId: "878576659959",
+  appId: "1:878576659959:web:f44fdc4c096627db6192f2",
+  measurementId: "G-YST5MR0LQ2"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+
+
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
